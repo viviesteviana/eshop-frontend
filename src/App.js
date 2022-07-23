2f2048fc9b8d40678dc05b749951def1ab3e8210
viviesteviana <viviestevianas@gmail.com>
2022-07-23 16:43:25 +0700

    latihan5

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {useEffect, useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Auth from './pages/Auth';
-import Carts from './pages/Carts';
-import Profile from './pages/Profile';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Navbar from "./components/Navbar";
+import Carts from "./pages/Carts";
+import Profile from "./pages/Profile";
 
-function App() { 
+function App() {
   const [isLogedIn, setIsLogedIn] = useState(false);
 
-useEffect(() => {
-  const token = localStorage.getItem('eshop_jwt');
-  if(token) {
-    setIsLogedIn(true);
-  }
-}, []);
+  useEffect(() => {
+    const token = localStorage.getItem("eshop_jwt");
+
+    if (token) {
+      setIsLogedIn(true);
+    }
+  }, []);
 
   return (
     <Router>
       <Navbar isLogedIn={isLogedIn} />
-      
+
       <Routes>
-        <Route path='/'  element={<Home/>} />
+        <Route path="/" element={<Home />} />
 
-        <Route path='/auth/login'  element={<Auth login setIsLogedIn={setIsLogedIn} />} />
-        <Route path='/auth/register' element={<Auth register />} />
+        <Route
+          path="/auth/login"
+          element={
+            <Auth isLogedIn={isLogedIn} login setIsLogedIn={setIsLogedIn} />
+          }
+        />
+        <Route
+          path="/auth/register"
+          element={<Auth isLogedIn={isLogedIn} register />}
+        />
 
-        <Route path='/Carts' element={<Carts isLogedIn={isLogedIn} />} />
-
-        <Route path='/profile' element={<Profile isLogedIn={isLogedIn} />} />
+        <Route path="/carts" element={<Carts isLogedIn={isLogedIn} />} />
+        <Route
+          path="/profile"
+          element={
+            <Profile setIsLogedIn={setIsLogedIn} isLogedIn={isLogedIn} />
+          }
+        />
       </Routes>
-
     </Router>
   );
 }
